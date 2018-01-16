eedf78abaa902af168965b59e4caaa80eb79edf9
mm042676 <mm042676@gmail.com>
2018-01-16 14:03:55 -0500

    chnaged to MVVM

--- Peekaboo Holdem/Peekaboo_Holdem/View/peekaboo.xaml.cs
+++ Peekaboo Holdem/Peekaboo_Holdem/View/peekaboo.xaml.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
-namespace Peekaboo_Holdem
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Peekaboo_Holdem.View
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class peekaboo : ContentPage
     {
-        public MainPage()
+        public peekaboo()
         {
             InitializeComponent();
         }
